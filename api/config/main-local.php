2b922854ab99cf89fdcdd9c2e501bac7f720f06f
Your Name <you@example.com>
2017-08-13 23:06:18 +0700

    Some git ignore fix for simplify deploy

---
+++ api/config/main-local.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'IIMe4dFwJTtz-1kz43sp4uxe1puEdWJ8',
+        ],
+    ],
+];
